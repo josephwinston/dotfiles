928f48e9628010c1a2cf1cf713e981c9694cb55a
Jody Winston <joseph.winston@halliburton.com>
2014-10-17 09:21:32 -0500

    Added 14.04

---
+++ home/.x86_64-unknown-linux-gnu/lgc.com/.calendar.el
@@ -0,0 +1,10 @@
+;; -*- Emacs-Lisp -*-
+
+;;
+;; $Id: //depot/Jody/main/jody/.i686-unknown-linux/xprtcc.com/.calendar.el#4 $
+;;
+
+(setq calendar-location-name "Houston, TX")
+(if (fboundp 'atan) (setq calendar-latitude 29.572084))
+(if (fboundp 'atan) (setq calendar-longitude -95.130292))
+
---
+++ home/.x86_64-unknown-linux-gnu/lgc.com/.emacs.24.el
@@ -0,0 +1,29 @@
+(setq auto-mode-alist
+	  (append
+	   '(("CMakeLists\\.txt\\'" . cmake-mode))
+	   '(("\\.cmake\\'" . cmake-mode))
+	   auto-mode-alist))
+
+(autoload 'cmake-mode "~/Tools/share/emacs/site-lisp/cmake-mode.el" t)
+
+;;
+;; scala
+;;
+
+(add-to-list 'load-path "~/Tools/share/emacs/site-lisp/scala")
+(require 'scala-mode-auto)
+
+(defun scala-turnoff-indent-tabs-mode ()
+  (setq indent-tabs-mode nil))
+
+;;
+;; scala mode hooks
+;;
+
+(add-hook 'scala-mode-hook 'scala-turnoff-indent-tabs-mode)
+
+;;
+;; magit
+;;
+
+
