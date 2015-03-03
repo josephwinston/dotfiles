5295abc30c34154ea1cb7423d840e3a637dbce91
Jody Winston <joseph.winston@halliburton.com>
2015-03-02 20:35:05 -0600

    Updated to x86_64-apple-darwin14.3.0

---
+++ home/.x86_64-apple-darwin14.3.0/lgc.com/.info.el
@@ -0,0 +1,15 @@
+;; -*- Emacs-Lisp -*-
+
+;;
+;; $Id: .info.el,v 1.1 2004/08/09 23:13:08 CVS_jody Exp $
+;;
+
+(require 'info)
+
+;;
+;; find local info nodes
+;;
+
+;(setq Info-directory-list
+;      (append Info-directory-list '("/usr/shar/info/")))
+
