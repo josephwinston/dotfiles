6de2f2fee38d1ebf3fdafa86d11ece9b55f99f47
Jody Winston <joseph.winston@halliburton.com>
2015-12-29 18:14:13 -0600

    Added .x86_64-apple-darwin15.2.0

---
+++ home/.x86_64-apple-darwin15.2.0/lgc.com/.calendar.el
@@ -0,0 +1,10 @@
+;; -*- Emacs-Lisp -*-
+
+;;
+;; $Id: .calendar.el,v 1.1 2004/08/09 23:13:08 CVS_jody Exp $
+;;
+
+(setq calendar-location-name "Houston, TX")
+(if (fboundp 'atan) (setq calendar-latitude 29.572084))
+(if (fboundp 'atan) (setq calendar-longitude -95.130292))
+
